31939eb1988c922bff3664d4a211a3599051e362
Camila <camilalr93@gmail.com>
2015-11-05 11:26:14 -0500

    Interfaz de notas. Split

---
+++ EasyNote/MainPage.xaml.cs
@@ -0,0 +1,87 @@
+﻿using EasyNote.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace EasyNote
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private ObservableCollection<MenuItem> menuList;
+
+        public ObservableCollection<MenuItem> MenuList
+        {
+            get
+            {
+                if (menuList == null)
+                {
+                    menuList = new ObservableCollection<MenuItem>();
+
+                    MenuItem item = new MenuItem() { Name = "Favoritos", Icon = "Favorite" };
+                    MenuItem item1 = new MenuItem() { Name = "Agregar", Icon = "Add" };
+                    MenuItem item2 = new MenuItem() { Name = "Editar", Icon = "Edit" };
+                    MenuItem item3 = new MenuItem() { Name = "Eliminar", Icon = "Delete" };
+
+                    menuList.Add(item);
+                    menuList.Add(item1);
+                    menuList.Add(item2);
+                    menuList.Add(item3);
+                }
+                return menuList;
+            }
+            set { menuList = value; }
+        }
+        private ObservableCollection<GaleryItem> galery;
+
+        public ObservableCollection<GaleryItem> Galery
+        {
+            get
+            {
+                if (galery == null)
+                {
+                    galery = new ObservableCollection<GaleryItem>();
+                    GaleryItem item = new GaleryItem() { Titulo = "Recordar", Contenido = "Lunes" };
+
+                    galery.Add(item);
+                }
+                return galery;
+            }
+            set { galery = value; }
+        }
+
+
+        private void showMenu(object sender, RoutedEventArgs e)
+        {
+            if (split.IsPaneOpen)
+            {
+                split.IsPaneOpen = false;
+            }
+            else
+                split.IsPaneOpen = true;
+
+        }
+
+    }
+}
---
+++ EasyNote/Models/GaleryItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyNote.Models
+{
+    public class GaleryItem
+    {
+        public string Titulo { get; set; }
+        public string Contenido { get; set; }
+    }
+}
